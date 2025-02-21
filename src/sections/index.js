769a38a14268e3796fdb410f19a9a667b75eb160
Rushi Shah <rushi2706@gmail.com>
2025-02-21 22:04:00 +0530

    image path

--- src/sections/index.js
+++ src/sections/index.js
@@ -2,17 +2,17 @@ const HomePageContent = [
     {
         title: 'Research Paper Writing',
         text: 'Join us to write Conference Level Research Papers with expert guidance and support',
-        image: './src/assets/images/reasearch1.png',
+        image: './src/assets/images/research1.png',
     },
     {
         title: 'Expert Consultation',
         text: 'Get personalized mentoring from PhD scholars and experienced researchers in your field',
-        image: './src/assets/images/reasearch2.png',
+        image: './src/assets/images/research2.png',
     },
     {
         title: 'Publication Support',
         text: 'Receive end-to-end assistance from paper writing to journal submission and review handling',
-        image: './src/assets/images/reasearch3.png',
+        image: './src/assets/images/research3.png',
     }
 ];
 
